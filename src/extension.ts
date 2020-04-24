77ff33a57a8e2dc674ad52a073b501dcd18f9af2
Sam Scott <samueltscott@gmail.com>
2020-04-24 23:50:34 +0100

    added tests to git tracking

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,8 @@ export function activate(context: vscode.ExtensionContext) {
 	let maxTerm = vscode.commands.registerCommand('maximizeterminal.maxTerm', () => {
 		// these commands are necessary to get the orange focus outline to appear in the right place
 		vscode.commands.executeCommand("workbench.action.toggleMaximizedPanel");
-		vscode.commands.executeCommand("workbench.action.terminal.toggleTerminal");
+		//vscode.commands.executeCommand("workbench.action.terminal.toggleTerminal");
+		vscode.commands.executeCommand("workbench.panel.terminal.focus");
 		vscode.commands.executeCommand("workbench.action.focusPanel");
 		vscode.commands.executeCommand("workbench.action.terminal.focus");
 	});
