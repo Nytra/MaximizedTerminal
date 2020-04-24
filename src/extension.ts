70d4d8d5a40bcfb5af14f170e4103856636c138f
Sam Scott <samueltscott@gmail.com>
2020-04-24 22:30:51 +0100

    replaced toggle terminal command with focus terminal command

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,9 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "maximizedterminal" is now active!');
+	console.log('Congratulations, your extension "maximizeterminal" is now active!');
 
-	let maxTerm = vscode.commands.registerCommand('maximizedterminal.maxTerm', () => {
+	let maxTerm = vscode.commands.registerCommand('maximizeterminal.maxTerm', () => {
 		// these commands are necessary to get the orange focus outline to appear in the right place
 		vscode.commands.executeCommand("workbench.action.toggleMaximizedPanel");
 		vscode.commands.executeCommand("workbench.action.terminal.toggleTerminal");
