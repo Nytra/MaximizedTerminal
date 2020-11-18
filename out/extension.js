26cb58e6442d33b943fb5602d0c50d4f9ce5373f
Sam Scott <samueltscott@gmail.com>
2020-11-18 00:32:38 +0000

    Fix terminal to not move the input line to the bottom of the screen after going from minimized to maximized

--- out/extension.js
+++ out/extension.js
@@ -13,34 +13,44 @@ exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 function activate(context) {
     return __awaiter(this, void 0, void 0, function* () {
-        // show message on first activation of the extension
+        // show interactive message on first activation of the extension
+        // for debugging
         //context.globalState.update("informationMessageShown", false);
+        // display information message if context's globalstate has key "informationMessageShown" with value: false
         if (!context.globalState.get("informationMessageShown")) {
             vscode.window.showInformationMessage("You can now use ctrl+' to maximize the terminal. This behaviour can be reconfigured in settings.", "Go to settings").then(selection => {
+                // on selection resolution, set key "informationMessageShown" to value: true (so that the message doesn't get shown again)
                 context.globalState.update("informationMessageShown", true);
                 if (selection === "Go to settings") {
+                    // go to settings
                     vscode.commands.executeCommand('workbench.action.openSettings', 'maximizeterminal.useAlternativeKeybinding');
                 }
             });
         }
+        // register command to maximize the terminal, store its disposable in a local variable (for subscribing to it later)
         let maxTerm = vscode.commands.registerCommand('maximizeterminal.openMaximizedTerminal', () => {
+            // sequence of commands to maximize the terminal. possibly non-optimal.
             vscode.commands.executeCommand("workbench.action.togglePanel");
             vscode.commands.executeCommand("workbench.action.toggleMaximizedPanel");
-            //vscode.commands.executeCommand("workbench.action.terminal.toggleTerminal");
-            // broken. possibly deprecated?
-            //vscode.commands.executeCommand("workbench.panel.terminal.focus"); 
-            //vscode.commands.executeCommand("workbench.action.focusPanel");
-            //vscode.commands.executeCommand("workbench.action.terminal.focus");
             vscode.commands.executeCommand("terminal.focus");
+            // call this to stop the terminal from moving the input line to the bottom when pressing a key after going from minimized to maximized state
+            vscode.commands.executeCommand("panel.focusPanel");
+            // broken. removed from api?
+            //vscode.commands.executeCommand("workbench.panel.terminal.focus"); 
+            //vscode.commands.executeCommand("workbench.action.terminal.focus");
             //let config = vscode.workspace.getConfiguration('maximizeterminal');
+            // output all api commands to console
             //vscode.commands.getCommands().then(onfulfilled => {console.log(onfulfilled);});
             //vscode.window.showInformationMessage("maxTerm called");
         });
         let minTerm = vscode.commands.registerCommand('maximizeterminal.openMinimizedTerminal', () => {
+            // sequence of commands to open a minimized terminal. again, possibly non-optimal.
+            // 'if it works and it ain't broke, don't fix it.'
             vscode.commands.executeCommand("workbench.action.togglePanel");
             vscode.commands.executeCommand("workbench.action.toggleMaximizedPanel");
             vscode.commands.executeCommand("workbench.action.toggleMaximizedPanel");
             vscode.commands.executeCommand("terminal.focus");
+            vscode.commands.executeCommand("panel.focusPanel");
             //vscode.window.showInformationMessage("minTerm called");
         });
         context.subscriptions.push(maxTerm);
